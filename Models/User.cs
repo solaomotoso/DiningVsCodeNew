076d76cf26edefdac39ae5a9b3f6771812e4bf44
olusola omotoso <solaomotoso2002@yahoo.com>
2023-06-16 13:15:07 +0100

    160623Usermodel

--- Models/User.cs
+++ Models/User.cs
@@ -3,6 +3,8 @@ namespace DiningVsCodeNew
     public class User
     {
         public int id { get; set; }
+        public int custTypeId { get; set; }
+        public string custId { get; set; }
         public string firstName { get; set; }
         public string lastName { get; set; }
         public string userName { get; set; }
